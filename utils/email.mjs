088701ee0f0df31a5236f7dba3f4134a4daa365b
Vishal <130888731+Vishalcd@users.noreply.github.com>
2024-10-14 16:46:58 +0530

    Update email.mjs

--- utils/email.mjs
+++ utils/email.mjs
@@ -20,7 +20,14 @@ export default class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      return 1;
+      return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+     });
     }
 
     return nodemailer.createTransport({
